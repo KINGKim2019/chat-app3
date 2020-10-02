4b7a1154ccde3bdc98a9071ef8705059b2922772
KINGKim2019 <71362518+KINGKim2019@users.noreply.github.com>
2020-10-02 17:19:00 +0900

    最終ミス確認

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
+   
   factory :room do
     name {Faker::Team.name}
   end
+ 
+
+    FactoryBot.define do
+      factory :room_user do
+      association :user
+      association :room
+      end
+    end
 end
